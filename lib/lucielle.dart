2ce955fa989dd700c868d640c45d09807bea2ede
Furkan Kazım Çam <dev.furkankazimcam@gmail.com>
2025-04-29 01:58:56 +0300

    try export

--- lib/lucielle.dart
+++ lib/lucielle.dart
@@ -1,3 +1,4 @@
 export 'package:lucielle/widget/text/luci_text.dart';
 export 'package:lucielle/widget/sized_box/empty_box.dart';
 export 'package:lucielle/widget/sized_box/space_box.dart';
+export 'package:lucielle/utils/extensions/index.dart';
--- lib/utils/extensions/index.dart
+++ lib/utils/extensions/index.dart
@@ -1,1 +1,3 @@
-export 'package:lucielle/utils/extensions/index.dart';
+export 'package:lucielle/utils/extensions/context_extension/media_query_extensions.dart';
+export 'package:lucielle/utils/extensions/int_extension/int_extension.dart';
+export 'package:lucielle/utils/extensions/string_extension/string_extension.dart';
